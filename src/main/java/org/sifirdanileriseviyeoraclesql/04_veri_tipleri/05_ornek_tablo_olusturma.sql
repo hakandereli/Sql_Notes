29b078091e33d6e42a1c52c1b50595c342377eab
hakan dereli <hakandereli28@gmail.com>
2023-09-18 13:48:25 +0300

    veri tiplerinini uygulayan örnek tablo oluşturuldu.

---
+++ src/main/java/org/sifirdanileriseviyeoraclesql/04_veri_tipleri/05_ornek_tablo_olusturma.sql
@@ -0,0 +1,69 @@
+/*Bu tablo veri tiplerini örnekleme için oluşturuldu mantık göz ardı edildi.*/
+
+CREATE TABLE PERSONEL(
+    PERSONEL_ID NUMBER,
+    FIRST_NAME CHAR(30),
+    LAST_NAME VARCHAR2(30),
+    JOB_TITLE VARCHAR2(15),
+    YEAR_OF_EXPERIENCE INTERVAL YEAR TO MONTH,
+    FLIGHT_TIME INTERVAL DAY TO SECOND,
+    APPROVAL_DATE TIMESTAMP,
+    HIRE_DATE DATE,
+    PRIMARY KEY (PERSONEL_ID)
+);
+
+/*TABLE in yapısını görmek için kullanılır. Description dan gelmektedir.*/
+DESC PERSONEL;
+
+/*Tabloya veri giriliyor. String değerler için tek '' tırnak içersinide yazılır*/
+
+/*TIMESTAMP ALAN İÇİN CURRENT_TIMESTAMP VE SYSTIMESTAMP*/
+
+/*TRUNC() TARİH KISMINI ALIR SADECE ZAMAN KISMINI ATAR*/
+
+/*insert örneği*/
+INSERT INTO PERSONEL(
+    PERSONEL_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    JOB_TITLE,
+    YEAR_OF_EXPERIENCE,
+    FLIGHT_TIME,
+    APPROVAL_DATE,
+    HIRE_DATE
+) VALUES
+    (
+    1,
+    'HAKAN',
+    'DERELİ',
+    'ORACLE dba',
+    INTERVAL '30-2' YEAR TO MONTH,
+    INTERVAL '16 11:09:08.555' DAY TO SECOND,
+    CURRENT_TIMESTAMP,
+    TRUNC(SYSDATE)
+    );
+
+    INSERT INTO PERSONEL(
+    PERSONEL_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    JOB_TITLE,
+    YEAR_OF_EXPERIENCE,
+    FLIGHT_TIME,
+    APPROVAL_DATE,
+    HIRE_DATE
+) VALUES
+    (
+    2,
+    'OSMAN',
+    'ULUSAN',
+    '.NET DEVOLOPER',
+    INTERVAL '8' MONTH,
+    INTERVAL '16 10:09:08.555' DAY TO SECOND,
+    SYSTIMESTAMP,
+    SYSDATE
+    );
+
+/*SORGULAMA*/
+
+SELECT * FROM PERSONEL;
