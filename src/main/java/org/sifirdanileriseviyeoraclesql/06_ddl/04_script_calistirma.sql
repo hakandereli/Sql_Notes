e58599761e53227680d3efc99bb0b8e8fa8f4547
hakan dereli <hakandereli28@gmail.com>
2023-09-27 00:20:32 +0300

    script calistirma

---
+++ src/main/java/org/sifirdanileriseviyeoraclesql/06_ddl/04_script_calistirma.sql
@@ -0,0 +1,13 @@
+/*
+    Sql developer da dosyayı open ile açıp çalıştırabilirsin.
+
+    Sql plus da ise
+    Start C:\pc\2_constraints veya
+    @C:\pc\2_constraints
+
+    ile tam yol vererek çalıştırabilirsin.
+
+    ER diagram ekranında sağ tık label dersen fk pk ların isimlerini de gösteriz.
+
+    gene sağ tık print er diagram ile çıktı da alabilirsin.
+*/
