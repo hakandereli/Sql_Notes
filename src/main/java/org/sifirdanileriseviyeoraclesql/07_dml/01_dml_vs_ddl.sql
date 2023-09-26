ed88682113f4a2f3ddba9b72f5c06df33a8dc842
hakan dereli <hakandereli28@gmail.com>
2023-09-27 00:24:07 +0300

    dml vs ddl

---
+++ src/main/java/org/sifirdanileriseviyeoraclesql/07_dml/01_dml_vs_ddl.sql
@@ -0,0 +1,7 @@
+/*
+    DDL komutları Create, Alter, Drop
+    DML Komutları Insert , Select, Update , Delete
+
+    DDL nesneler üzerinde kullanılan komut gruplarıdır.
+    DML veriler üzerinde kullanılan komut gruplarıdır.
+*/
