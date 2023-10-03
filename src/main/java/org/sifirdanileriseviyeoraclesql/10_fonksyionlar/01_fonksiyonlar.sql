0cd4c25764559ba48b0c46a4067a4690f3eeadc5
hakan dereli <hakandereli28@gmail.com>
2023-10-03 15:30:44 +0300

    sayisal fonksiyonlar incelendi

---
+++ src/main/java/org/sifirdanileriseviyeoraclesql/10_fonksyionlar/01_fonksiyonlar.sql
@@ -0,0 +1,15 @@
+/*
+ORACLE KURULDUĞUNDA hazır gelen fonksiyonlar vardır.
+Kendin de fonksiyon yazabilirsin. ama bu plsql konusuna girer
+*/
+
+/*Hazır Fonksiyonlar : Geri Dönüş Tiplerine ve Kullanım amaçlarına göre gruplandırılır.*/
+
+/*
+    Geri Dönüş Tipine Göre
+    1-Sayısal Fonksiyonlar
+    2-Karakter Fonksiyonlar
+    3-Tekil Sonuç Fonksiyonları
+    4-İstatistik Fonksiyonları
+    5-Tarih ve Zaman Fonksiyonları
+*/
