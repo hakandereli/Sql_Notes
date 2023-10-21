1ba89848ab897fcf000c620a1a75969f0595bb1e
hakandereli <hakandereli28@gmail.com>
2023-10-21 21:22:04 +0300

    joinler ve farkları incelendi

---
+++ src/main/java/org/sifirdanileriseviyeoraclesql/11_joins/01_joins.sql
@@ -0,0 +1,32 @@
+/*Birden fazla tablodan kayıt sorgulamak için,
+sorguda kullanılacak tabloların kolonları arasında ilişki(reletain kurarak sorgulamaya, tablolar arasında JOIN denir.)
+  */
+
+/*Join Türleri
+
+Natural Join
+Inner Join
+Outer Join
+  Left Outer Join
+  Right Outer Join
+  Full Outher Join
+Self Join
+Cross Join
+*/
+
+
+/*İlişkili tablolardaki kayıtları tek bir tabloymuş gibi listelemek için kullanılır.*/
+
+/*JOIN - SYNTAX(ANSI FORMAT)
+src\main\resources\img\sifirdanileriseviyeoraclesql\11_joins\01_join syntax.png
+*/
+
+/*left right join de sağ sol mevzusu yazım sırası ile alakalıdır
+
+  self join tablonun kendisi ile joinlenmesi demektir.
+
+  cross join kartezen çarpım demektir.
+  a çarpan b AxB gibi
+ src\main\resources\img\sifirdanileriseviyeoraclesql\11_joins\02_join_tipleri.png
+ src\main\resources\img\sifirdanileriseviyeoraclesql\11_joins\03_ansi_ve_geleneksel_format_join yazılması.png
+*/
