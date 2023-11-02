de5ccb5ab04b1dbe541a9110bab302f2922de4b1
hakandereli <hakandereli28@gmail.com>
2023-11-03 00:15:23 +0300

    plsql içerisinde java kullanma, ek özellikler avantajlar ve  terminoloji incelendi.

--- src/main/java/org/plsql/01_giris/01_genel_bilgiler.sql
+++ src/main/java/org/plsql/01_giris/01_genel_bilgiler.sql
@@ -7,4 +7,41 @@ sql programalama dilinin bir dizi kurallarla, yani prosedür özellikleriyle don
 
   Oracle Veritabanına yerleştirilen üç temel programlama dili vardır.
 
+  1- SQL
+  2- JAVA
+  3- PL/SQL
+
+  plsql içerisinde sql kullanılır ancak çok daha gelişmiştir.
+
+  Veritabanı seviyesinde programlama dilidir. (Database levet back-end programming)
+
+  Plsql -> Java , C#, Pyton , PHP vs .. gibi sql çalıştırılan programlama dillerinde çalıştırılabilir.
+
+  Java class içerisinde örneğin bir function çağırabilirsin. Örneği görsel de mevcut. JDBC olabildiği gibi
+  spring boot içerisinde de örneğin @Query anatasyonu ile vs de olabilir örnekleri internette mevcut.
+
+  java - plsql.png
+
+
+  ***********************PLSQL İÇERİSİNDE JAVA CLASS ÇALIŞTIRILABİLİR.*******************************
+  Görseli mevcut java - plsql.png
+
+Plsql konusu kimleri ilgilendiriyor ?
+
+  RDBMS mimaride veritabanı kullanıcaksa, modüler, performanslı ve bakım-onarım maliyeti düşük olan uygulama
+  geliştirmek isteyen herkesi ilgilendiriyor.
+
+  plsql bilen yazılımcı çok daha hızlı çalışacak uygulamalar geliştirebilir.
+
+  plsql avantajları görselde
+  plsql_avantajlari.png
+    Bu görseldeki aynı anda farklı programlardan çalıştırılabilir demek birbirini bloklamaz yani java ve c# da diyelim
+  2 programın var ikisi de aynı anda bu db deki plsql i çalıştırabilir.
+
+
+  Terminoloji
+  PL/SQL (ORACLE DATABASE)
+  T-SQL (MsSQL DATABASE)
+  PL/pgSQL (PostgreSQL DATABASE)
+  VS..
 */
