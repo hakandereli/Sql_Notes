919217e54d0b68119632e2d2b59c2112693cd4d0
hakandereli <hakandereli28@gmail.com>
2023-11-02 23:45:26 +0300

    plsql giris

---
+++ src/main/java/org/plsql/01_giris/01_genel_bilgiler.sql
@@ -0,0 +1,10 @@
+/*Plsql oracle Veritabanı programlama demektir.
+  Procedural Language extension to Structured Query Language
+
+Plsql kullanılmadan geliştirilen bir sistem de veritabanı sadece depolama için kullanılıyor denilebilir.
+
+sql programalama dilinin bir dizi kurallarla, yani prosedür özellikleriyle donatılmış halidir.
+
+  Oracle Veritabanına yerleştirilen üç temel programlama dili vardır.
+
+*/
